4c6c41903da46d673ca39452207b99fb8a354953
David Gerdes <gerdes@umich.edu>
2015-09-02 15:55:03 -0400

    updates

--- KBO.py
+++ KBO.py
@@ -244,50 +244,10 @@ def exposure_midpoint(obs, field):
     return ephem.date(obs.date + nstack*ephem.second*obs.exptime/2)
 
 
-def t_opp(ra, dec):
-    '''
-    Computes the approximate time of opposition for the given (ra, dec)
-    '''
-    t0 = ephem.date('2015/01/01')
-    lon, lat = Ecliptic(Equatorial(ra, dec)).get()
-    sun = ephem.Sun()
-    sun.compute(t0)
-    lon_s, lat_s = Ecliptic(Equatorial(sun.ra, sun.dec)).get()
-    delta_lon = lon-lon_s
-    # first approximation
-    t180 = t0 + sidereal_year/2 + delta_lon/sidereal_rate
-    # correction
-    sun.compute(t180)
-    lon_s, lat_s = Ecliptic(Equatorial(sun.ra, sun.dec)).get()
-    t180 = t180 + (lon-lon_s + np.pi)/sidereal_rate
-    return ephem.date(t180)
-
-def parallax(ra, dec, date):
-    '''
-    
-    Calculate the shifts and rate of change in ecliptic coordinates expected for a stationary object 
-    at the specified location on the specified date.
-    
-    date should be a DateTime object, pyEphem
-    date, floating-point JD, or compatible string.
-    Results are returned as a pair (dlon, dlat),
-    in arbitrary units. To get appropriately scaled
-    results, multiply by the reciprocal of the object's
-    distance from the sun in AU.
-    '''
-    date = DateTime(date)
-    omegat = sidereal_rate * (date - t_opp(ra, dec))
-    lat = Ecliptic(Equatorial(ra, dec)).lat
-    dlon = -np.sin(omegat)/np.cos(lat)
-    dlat = np.cos(omegat)*np.sin(lat)
-    vlon = -np.cos(omegat)/np.cos(lat)
-    vlat = -np.sin(omegat)*np.sin(lat)
-    return dlon, dlat, vlon, vlat
-
-def exposure_parallax():
-    p={}
-    for exp in exposures:
-        dlon, dlat, vlon, vlat = parallax(exp.ra, exp.dec, exp.date)
-        p[exp.expnum]={'dlon':dlon, 'dlat':dlat, 'vlon':vlon, 'vlat':vlat}
-    return p
+#def exposure_parallax():
+#    p={}
+#    for exp in exposures:
+#        dlon, dlat, vlon, vlat = parallax(exp.ra, exp.dec, exp.date)
+#        p[exp.expnum]={'dlon':dlon, 'dlat':dlat, 'vlon':vlon, 'vlat':vlat}
+#    return p
 
